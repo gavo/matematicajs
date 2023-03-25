33bccb8ff2b1726794f125fce5efc59bf200fc3e
gavo <gvmgust@gmail.com>
2023-03-25 00:52:07 -0400

    Class 4: Triangle Height

--- math.js
+++ math.js
@@ -41,6 +41,16 @@ const getCircle = (circleRadio) => {
   };
 };
 
+const getTriangleHeight = (side, base) => {
+  if (base === side) {
+    return new Throw("This is not an isosceles Triangle");
+  }
+  return Math.sqrt(Math.pow(side, 2) - Math.pow(base, 2) / 4);
+};
+
 console.log(getSquare(8));
 console.log(getTriangle(4, 5, 6, 5));
 console.log(getCircle(3));
+console.log("Isosceles heigh", getTriangleHeight(6, 6));
+
+console.end;
