bd5580d16ea18fdac73d694bd22ff5b962ff9f44
gavo <gvmgust@gmail.com>
2023-03-27 16:18:18 -0400

    Class 17-18: Calculando Moda

--- platzimath.js
+++ platzimath.js
@@ -1,4 +1,6 @@
-const list = [13, 56, 23, 22, 66, 32, 12, 63, 74, 12];
+const list = [5, 10, 15, 20, 20, 21];
+
+const esPar = (list) => list.length % 2 === 0;
 
 const averageForeach = (list) => {
   let total = 0;
@@ -11,4 +13,42 @@ const averageForeach = (list) => {
 const averageReduce = (list) =>
   (list.reduce((sum, val) => sum + val, 0) / list.length).toFixed(2);
 
-console.log(list, averageForeach(list), averageReduce(list));
+const calculateMedian = (list) => {
+  const aux = Object.assign([], list);
+  aux.sort((a, b) => a - b);
+  if (esPar(list)) {
+    return ((aux[aux.length / 2] + aux[aux.length / 2 - 1]) / 2).toFixed(2);
+  }
+  return aux[Math.floor(aux.length / 2)];
+};
+
+const calculateMode = (list) => {
+  const result = {};
+  list.forEach((el) => {
+    if (result[el]) {
+      result[el] += 1;
+    } else {
+      result[el] = 1;
+    }
+  });
+  let mode = null;
+  Object.keys(result).forEach((el) => {
+    if (!mode || result[el] > result[mode]) {
+      mode = el;
+    }
+  });
+  return mode;
+};
+
+console.log(
+  "list:",
+  list,
+  "\naverage:",
+  averageForeach(list),
+  "\naverage reduce:",
+  averageReduce(list),
+  "\nmedian:",
+  calculateMedian(list),
+  "\nmode:",
+  calculateMode(list)
+);
